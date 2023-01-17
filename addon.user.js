a41f081b6e6a253bffe01c3d929503dfb2fb902f
WestKite <talk@kakao.one>
2023-01-17 15:09:48 +0900

    Add files via upload

--- addon.user.js
+++ addon.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         	Cafe24_AutoDeliveryCheck
 // @namespace	    http://*.cafe24.com/
-// @version      	0.3
+// @version      	20230117-1
 // @description	    Cafe24에서 배송 중에 있는 상품들을 자동으로 완료 처리 합니다.
 // @author	        Seoyeon Bae
 // @match	        https://*.cafe24.com/admin/php/shop1/s/shipped_end_list.php*
@@ -11,6 +11,7 @@
 // @run-at	        document-idle
 // ==/UserScript==
 
+var ignore_count=0;
 var Carrier_list = Start();
 var checkbox_array = new Array();
 $('#searchResultList > tbody > tr > td.cellMerge.eCellHeight > div > table > tbody > tr > td:nth-child(1) > input.chkbox').each(function(index, item) {
@@ -21,6 +22,11 @@ function checkStatus() {
     $('[id*="eDlvrTrace"]').closest("td").each(function(index, item) {
         let user_carr = $(item).text().trim().split(' ').join('').split('\n');
 
+        if(typeof getCarrierByCode(user_carr[0])[0] == 'undefined'){
+            $(item).append('<br><span style="color: rgb(153, 0, 204); font-weight: bold;">미지원 택배사</span>');
+            ignore_count = ignore_count+1;
+            return true;
+        }
         let req_carr_id = getCarrierByCode(user_carr[0])[0].id;
         let url = 'https://apis.tracker.delivery/carriers/' + req_carr_id + '/tracks/' + user_carr[3].replaceAll('-','');
         console.log(url);
@@ -35,13 +41,13 @@ function checkStatus() {
                     console.log(user_carr[3] + json['state']['text']);
                     if (json['state']['text'].search("완료") != -1) {
                         $(item).append('<br><span style="color: rgb(0, 124, 14); font-weight: bold;">' + json['state']['text'] + '</span>');
-                        $(checkbox_array[index]).attr('checked', 'true');
+                        $(checkbox_array[index+ignore_count]).attr('checked', 'true');
                     } else {
                         $(item).append('<br><span style="color: rgb(255, 152, 0); font-weight: bold;">' + json['state']['text'] + '</span>');
                     }
                 }
             } else {
-                $(item).append('<br><span style="color: rgb(166, 0, 0); font-weight: bold;">' + '운송장오류' + '</span>');
+                $(item).append('<br><span style="color: rgb(166, 0, 0); font-weight: bold;">' + '조회내역 없음' + '</span>');
             }
         };
         apixhr.send();
