b58fe7b0de43bbe4bf7db1bb7a080ae3fbc05da2
WestKite <talk@kakao.one>
2023-04-20 11:02:14 +0900

    async Path
    ㅈㄱㄴ

--- smartlog_google.js
+++ smartlog_google.js
@@ -1,59 +1,61 @@
 // ==UserScript==
-// @name	SMARTLOG_GOOGLE.JS
+// @name		SMARTLOG_GOOGLE.JS
 // @namespace	https://smlog.co.kr
-// @version	20230306.01
+// @version		20230420.01
 // @description	스마트로그 실시간방문자에서 구글쇼핑탭 유입시 상품명으로 치환
-// @author	Seoyeon Bae
-// @match	*://smlog.co.kr/hmisNew/vsl_live.html*
-// @icon	https://www.google.com/s2/favicons?sz=64&domain=smlog.co.kr
+// @author		Seoyeon Bae
+// @match		*://smlog.co.kr/hmisNew/vsl_live.html*
+// @icon		https://www.google.com/s2/favicons?sz=64&domain=smlog.co.kr
 // @updateURL	https://github.com/bsy0317/Cafe24-Utility/raw/main/smartlog_google.js
 // @downloadURL	https://github.com/bsy0317/Cafe24-Utility/raw/main/smartlog_google.js
-// @run-at	document-idle
+// @run-at		document-idle
 // ==/UserScript==
 
 let pServer = 'https://krr.kr:6405';
-let hostServer = 'chingchanmall.com';
+let hostServer = 'm.chingchanmall.com';
+var acceessableCount = 1;
 
-async function codeToName(code){
-	let parseCode = code.replace('cafe24_ccnaramall_1_','');
-	await $.ajax({
-	  method: 'GET',
-	  url: pServer+'/'+hostServer+'/product/detail.html?product_no='+parseCode,
-	  dataType: 'html',
-	  success: (res) => {
-	   let product_name = res.split("var product_name = '")[1].split("';")[0];
-	   let regexStr = /cafe24_ccnaramall_1_\d+/g;
-	   let itemArray = $('[id^="p_row"]').toArray();
-	   for(let i=0; i<itemArray.length; i++){
-		   let priv_html = $(itemArray[i]).html();
-		   $(itemArray[i]).html(priv_html.replaceAll(code, product_name));
-	   }
-	  },
-	  error: (res) => {
-        switch(res.status) {
-          case 404 :
-            $(itemArray[i]).html(priv_html.replaceAll(code, parseCode+'/404 Error'));
-            break;
-          case 500 :
-            $(itemArray[i]).html(priv_html.replaceAll(code, parseCode+'/500 Error'));
-            break;
-		  default:
-			$(itemArray[i]).html(priv_html.replaceAll(code, parseCode+'/'+res.status+' Error'));
-			break;
+function codeToName(code) {
+    let parseCode = code.replace('cafe24_ccnaramall_1_', '');
+    $.ajax({
+        method: 'GET',
+        url: pServer + '/' + hostServer + '/product/detail.html?product_no=' + parseCode,
+        dataType: 'html',
+        async: true, //비동기 여부
+		timeout: 1000,
+        success: function(res) {
+			console.log("ajax success");
+            let product_name = res.split("var product_name = '")[1].split("';")[0];
+            let regexStr = /cafe24_ccnaramall_1_\d+/g;
+            let itemArray = $('[id^="p_row"]').toArray();
+            for (let i = 0; i < itemArray.length; i++) {
+				if($(itemArray[i]).text().search(code) != -1){
+					let priv_html = $(itemArray[i]).html();
+					$(itemArray[i]).html(priv_html.replaceAll(code, product_name));
+					break;
+				}
+            }
         }
-      }
-	});
+    })
 }
 
-async function core(){
+function core() {
+	console.log("START");
 	let regexStr = /cafe24_ccnaramall_1_\d+/g;
-	$('[id^="p_row"]').each(async function(index, item){
+	$('[id^="p_row"]').each(function(index, item) {
 		let matches = $(item).html().match(regexStr);
-		if(matches != null && matches.length > 1){
+		if (matches != null && matches.length > 1) {
 			matches = matches[0];
-			await codeToName(matches);
+			codeToName(matches);
 		}
 	});
+	console.log("OK");
 }
 
-setInterval(core,1000);
+setInterval(function(){
+	if(acceessableCount > 0){
+		acceessableCount = acceessableCount - 1;
+		core();
+		acceessableCount = acceessableCount + 1;
+	}
+}, 1000);
